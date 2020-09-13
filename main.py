c2ebf205de333de39c67b1e41bdb66ec7286efaa
130 <crestel.leopold@gmail.com>
2020-09-13 20:19:29 +0200

    Fix: deal with tied notes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from pymxml.pymxml import mxml_read
 
 if __name__ == '__main__':
-    notes_list, mxml_elements = mxml_read('test.xml')
+    notes_list, mxml_elements = mxml_read(
+        'xmlsamples/ActorPreludeSample.musicxml')
+    
--- pymxml/pymxml.py
+++ pymxml/pymxml.py
@@ -16,8 +16,9 @@ def read_score(score, subdivision):
         score_soundingPitch = score
 
     # Output
-    notes = {}
-    elements = {}
+    notes_dict = {}
+    id_to_m21 = {}
+    tied_notes = {}
     id = 0
     for part in score_soundingPitch.parts:
         elements_iterator = part.flat.notes
@@ -26,24 +27,58 @@ def read_score(score, subdivision):
             # Need to quantize at some point...
             offset = element.offset
             duration = element.duration.quarterLength
-            offset_quantized = int(round(offset) * subdivision)
+            offset_quantized = int(round(offset) * subdivision) // subdivision
+
             if element.isChord:
-                for note in element._notes:
-                    pitch, velocity = get_note(note)
-                    add_note(notes, pitch, velocity, offset, offset_quantized, duration, instrument, id)
-                    elements[id] = note
-                    id = id + 1
+                m21_notes = element._notes
             else:
-                pitch, velocity = get_note(element)
-                add_note(notes, pitch, velocity, offset, offset_quantized, duration, instrument, id)
-                elements[id] = element
+                m21_notes = [element]
+
+            for m21_note in m21_notes:
+                pitch, velocity = get_pitch_velocity(m21_note)
+                this_note = {
+                    'offset': offset,
+                    'offset_quantized': offset_quantized,
+                    'duration': duration,
+                    'pitch': pitch,
+                    'velocity': velocity,
+                    'instrument': instrument,
+                    'mxml_ids': [id],
+                    'id': id
+                }
+                # check for ties
+                write = True
+                if m21_note.tie:
+                    write = False
+                    if m21_note.tie.type == 'start':
+                        tied_notes[pitch] = this_note
+                    else:
+                        # find the tied notes and append duration
+                        if pitch in tied_notes:
+                            tied_notes[pitch]['duration'] += duration
+                            tied_notes[pitch]['mxml_ids'].append(id)
+                        else:
+                            raise Exception('Trying to tie a note which does not have a starting tie')
+                        if m21_note.tie.type == 'stop':
+                            this_note = tied_notes[pitch]
+                            tied_notes.pop(pitch, None)
+                            write = True
+                    
+                # Write in list of notes
+                if write:
+                    if this_note['offset_quantized'] in notes_dict.keys():
+                        notes_dict[this_note['offset_quantized']].append(this_note)
+                    else:
+                        notes_dict[this_note['offset_quantized']] = [this_note]
+                id_to_m21[id] = m21_note
                 id = id + 1
+
     # Transform to the correct format
-    offset_quantized_list = sorted(notes.keys())
+    offset_quantized_list = sorted(notes_dict.keys())
     notes_list = []
     for time in offset_quantized_list:
-        notes_list.append(notes[time])
-    return notes_list, elements
+        notes_list.append(notes_dict[time])
+    return notes_list, id_to_m21
 
 
 def note_to_midiPitch(note):
@@ -58,26 +93,9 @@ def note_to_midiPitch(note):
     return octave * 12 + pc
 
 
-def get_note(note):
+def get_pitch_velocity(note):
     velocity = note.volume.velocity
     if velocity is None:
         velocity = 128
     pitch = note_to_midiPitch(note)
-    if note.tie and not(note.tie.type == 'start'):
-        raise Exception()
     return pitch, velocity
-
-def add_note(notes, pitch, velocity, offset, offset_quantized, duration, instrument, id):
-    this_note = {
-                    'offset': offset,
-                    'duration': duration,
-                    'pitch': pitch,
-                    'velocity': velocity,
-                    'instrument': instrument,
-                    'id': id
-                }
-    if offset_quantized in notes.keys():
-        notes[offset_quantized].append(this_note) 
-    else:
-        notes[offset_quantized] = [this_note]
-    return notes
