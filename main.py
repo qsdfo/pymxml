3970044d4b54daba0231c8bc7be9e3dacd4c7f1e
130 <crestel.leopold@gmail.com>
2020-09-23 12:49:52 +0200

    Read/Write ok

--- main.py
+++ main.py
@@ -10,35 +10,16 @@ if __name__ == '__main__':
 
     #########################
     # Randomly adding colours for testing
+    notes_list_coloured = []
     for notes in notes_list:
+        notes_coloured = []
         for note in notes:
             colour = random.choice(['red', 'blue', 'green'])
-            note['colour'] = colour
+            note_coloured = note
+            note_coloured['colour'] = colour
+            notes_coloured.append(note_coloured)
+        notes_list_coloured.append(notes_coloured)
     #########################
 
-    modified_score = mxml_write(m21_score, notes_list)
+    modified_score = mxml_write(m21_score, notes_list_coloured)
     modified_score.write(fp='out_test_color.mxml', fmt='musicxml')
-
-    # score_flat = score.flat
-    # for element in score_flat.elements:
-    #     if (hasattr(element, 'isNote') and element.isNote)\
-    #         or (hasattr(element, 'isChord') and element.isChord):
-    #         identifier = element.id
-    #         score_flat.getElementById(element.id).style.color = 'red'
-
-    # score = music21.converter.parse(filepath)
-    # for part in score.parts:
-    #     part_flat = part.flat        
-    #     elements_iterator = part_flat.notes
-    #     instrument = part.partName
-    #     for element in elements_iterator:
-    #         if element.isChord:
-    #             m21_notes = element._notes
-    #             identifier = element.id
-    #             for chord_index, m21_note in enumerate(m21_notes):
-    #                 part_flat.getElementById(identifier)[chord_index].style.color = 'red'
-    #         else:
-    #             m21_notes = element
-    #             identifier = element.id
-    #             part_flat.getElementById(identifier).style.color = 'red'
-    # score.write(fp='out_test_color.mxml', fmt='musicxml')
